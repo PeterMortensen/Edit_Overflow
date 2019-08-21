04c924d770724ae03e91a31b6fd6d8c7cecbbf9e
PeterMortensen <penge@pmortensen.eu>
2019-08-21 20:46:43 +0200

    Added an empty class as preparation for some refactoring of dumpWordList_asHTML() in WikipediaLookup.cs.

---
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/main/HTML_builder.cs
@@ -0,0 +1,30 @@
+﻿/****************************************************************************
+ * Copyright (C) 2019 Peter Mortensen                                       *
+ * This file is part of OverflowHelper.                                     *
+ *                                                                          *
+ *                                                                          *
+ * Purpose: XXXX                                                            *
+ *                                                                          *
+ ****************************************************************************/
+
+
+namespace OverflowHelper.core
+{
+
+    
+    class HTML_builder
+    {
+
+
+        public HTML_builder()
+        {
+
+        }
+
+
+    } //class HTML_builder
+
+
+}
+
+
