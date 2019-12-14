5d4f64da78bc4bea3a88166077775611410aacbe
Peter Mortensen (XP64_NEW) <penge@pmortensen.eu>
2019-12-15 00:56:23 +0100

    Web, main functionality (lookup): Refactored some of the deployment-specific part (for isolating this part), step 1.

--- Web_Application/EditOverflow.php
+++ Web_Application/EditOverflow.php
@@ -90,11 +90,37 @@
         <h1>(Note: PoC, to be styled to escape the 1990s...)</h1>
 -->
 
-        <h1>Edit Overflow v. 1.1.49a3 2019-11-28T193537</h1>
+        <h1>Edit Overflow v. 1.1.49a4 2019-12-14T235446</h1>
 
         <?php
             # For "Notice: Undefined variable: ..."
             error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+
+            # Mostly to isolate the deployment-specific part - there
+            # is a reason for it not taking a parameter...
+            #
+            # The return value is a PDO instance
+            #
+            function connectToDatabase()
+            {
+	            # Note:
+	            #
+	            #  "charset=utf8" is required for e.g. lookup of "lets" to work -
+	            #  where 'correctTerm' contains the Quora quote character,
+	            #  "U+2019  E2 80 99  RIGHT SINGLE QUOTATION MARK".
+	            #
+	            #  If not, the resulting content of 'correctTerm' is
+	            #  "U+FFFD  EF BF BD  REPLACEMENT CHARACTER".
+	            #
+	            $somePDOinstance = new PDO(
+	              #'mysql:host=mysql19.unoeuro.com;dbname=pmortensen_eu_db',
+	              'mysql:host=mysql19.unoeuro.com;dbname=pmortensen_eu_db;charset=utf8',
+	              'pmortensen_eu',
+	              '__pw_toBeFilledInDuringDeployment_'); # Sample key: 2019-09-16
+
+                return $somePDOinstance;
+            }
 
 
             # Escape problem "fix" (ref. <https://stackoverflow.com/a/33604648>)
@@ -153,21 +179,7 @@
             # For debugging
             #echo "<p>CustomerSQL: >>>$CustomerSQL<<< </p>\n";
 
-
-            # Note:
-            #
-            #  "charset=utf8" is required for e.g. lookup of "lets" to work -
-            #  where 'correctTerm' contains the Quora quote character,
-            #  "U+2019  E2 80 99  RIGHT SINGLE QUOTATION MARK".
-            #
-            #  If not, the resulting content of 'correctTerm' is
-            #  "U+FFFD  EF BF BD  REPLACEMENT CHARACTER".
-            #
-            $pdo = new PDO(
-              #'mysql:host=mysql19.unoeuro.com;dbname=pmortensen_eu_db',
-              'mysql:host=mysql19.unoeuro.com;dbname=pmortensen_eu_db;charset=utf8',
-              'pmortensen_eu',
-              '__pw_toBeFilledInDuringDeployment_'); # Sample key: 2019-09-16
+            $pdo = connectToDatabase();
 
             if (0)
             {
