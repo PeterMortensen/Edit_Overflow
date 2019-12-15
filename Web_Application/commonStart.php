7316410c7e7adfc523903611a6d8c57c16c40802
Peter Mortensen (XP64_NEW) <penge@pmortensen.eu>
2019-12-15 01:21:09 +0100

    Web, main functionality (lookup): Refactored some of the deployment-specific part (for isolating this part), step 2.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -4,17 +4,26 @@
   ###########################################################################
   # WordPress specific!
 
-  # For get the styling and other redundant content (like headers)
-  # from WordPress.
+  # For getting the styling and other redundant
+  # content (like headers) from WordPress.
   #
-  # So now we have a dependency on WordPress.
+  # So now we have a dependency on WordPress...
   #
   define('WP_USE_THEMES', false);
   require(dirname(__FILE__) . '/wp-blog-header.php');
 
-  get_header(); # Note: Using some WordPress themes results in
+  get_header(); # Note: Using some WordPress themes results in the following
+                #       on the page itself (though we also have it in the
+                #       title for all themes - but this is less intrusive):
                 #
-                #       Some that do not give
+                #           "Page not found"
+                #
+                #       Some themes that do not give it are:
+                #
+                #           "Responsive"    (the one we currently use)
+                #           "Orfeo"
+                #           "Hestia"
+                #           "Astra"
 
 ?>
 
