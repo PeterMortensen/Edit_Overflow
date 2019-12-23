f356669f348d65fa91e7715958d51fa4e2e815e5
Peter Mortensen (Hadsten) <penge@pmortensen.eu>
2019-12-23 15:01:28 +0100

    Web, footer: 1) Removed keyboard shortcuts for most items in the footer (as they often conflicted with keyboard shortcuts for the primary content, took up valuable keyboard shortcuts, were seldomly used, and there were other means to use keyboard shortcuts for them (e.g. in Firefox with mouseless browsing - <https://superuser.com/questions/50461>)). Note that removal for the HTML validation items awaits refactoting of that. 2) Replaced the now unused title tag (had keyboard shortcuts hints) with some documentation.

--- Web_Application/eFooter.php
+++ Web_Application/eFooter.php
@@ -1,6 +1,6 @@
 <?php
     # File: eFooter.php
- 
+
     # Purpose: Output of common footer for Edit Overflow for Web.
 
 
@@ -25,45 +25,38 @@
 
             <a
                 href="EditOverflow.php"
-                accesskey="E"
-                title="Shortcut: Shift + Alt + E"
+                title="Main functionality: Look up (incorrect) terms"
             >Edit Overflow</a>.
 
             <a
                 href="Text.php"
-                accesskey="J"
-                title="Shortcut: Shift + Alt + J"
+                title="Various text transformations, e.g. replacing TABs and removing trailing space."
             >Text stuff</a>.
 
             <a
                 href="Link_Builder.php"
-                accesskey="B"
-                title="Shortcut: Shift + Alt + B"
+                title="Convenient formatting of links in HTML, Markdown, and MediaWiki (Wikipedia)."
             >Link builder</a>.
 
             <a
                 href="FixedStrings.php"
-                accesskey="F"
-                title="Shortcut: Shift + Alt + F"
+                title="Often-used symbols and other content for easy copy-pasting, e.g. the degrees and micro symbols."
             >Fixed strings</a>.
 
             <a
                 href="EditSummaryFragments.php"
-                accesskey="D"
-                title="Shortcut: Shift + Alt + D"
+                title="Often-used content for edit summaries. Converts the most common error by both native speakers and ESLers."
             >Edit summary fragments</a>.
 
 
             <a
                 href="https://pmortensen.eu/EditOverflow/_Wordlist/EditOverflowList_2019-11-01.html"
-                accesskey="W"
-                title="Shortcut: Shift + Alt + W"
+                title="The full Edit Overflow wordlist, about 10,000 incorrect terms."
             >Word list</a>.
 
             <a
                 href="myInfo.php"
-                accesskey="I"
-                title="Shortcut: Shift + Alt + I"
+                title="Internal info for PHP (phpinfo())."
             >Environment information</a>.
 
             <!-- Note: PHP still works inside HTML comments, so we
