PeterMortensen/Edit_Overflow

commit 976a80fcc9bffdab8300cea53f26f823c0eacbe4

Web: 1) Moved information about the GET parameter to bypass WordPress from PHP comments to HTML comments (so we can easily accesss this information by viewing HTML source in a web browser). 2) The text stuff page now also work in native mode (e.g. <https://pmortensen.eu/world/Text.php?OverflowStyle=Native>)

Web_Application/commonStart.php
@@ -1,6 +1,4 @@
-
 <?php
-
     # Purposes (though we should probably split the WordPress-specific
     #           parts into a separate file, as this file has now
     #           taken on more responsibilities):
@@ -14,7 +12,11 @@
     #
     #   2. Centralise things (by means of helper functions) to eliminate
     #      some redundancy (e.g. Edit Overflow version).
-    #
+
+
+    # Only used by one page (Text.php)
+    const MAINTEXT = 'someText';
+    $formDataSizeDiff = -1;
 
 
     # Including version number and date
@@ -25,8 +27,9 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a5 2019-12-18T125312Z+0";
+        return "Edit Overflow v. 1.1.49a5 2019-12-18T200610Z+0";
     }
+
 
     # Note that we are using the WordPress convention of
     # name prefixing functions (with "the_") that echo's.
@@ -34,18 +37,24 @@
     function the_EditOverflowHeadline($aHeadline)
     {
         echo "<h1>$aHeadline - " . get_EditOverflowID() . "</h1>";
+
+        # Another side-effect of this function... Use the
+        # opportunity as this function is used by
+        # all pages (and in the beginning).
+        adjustForWordPressMadness();
     }
 
 
-    # Helper function to support switching between WordPress and
-    # native 1990s HTML (the original version)
+    # Helper function to support switching between WordPress
+    # and native 1990s HTML (the original version)
     #
     function useWordPress()
     {
-        # That is, if we pass OverflowStyle (by GET or POST), we can turn 
-        # off the WordPress part (e.g. to ease HTML validation (for 
-        # example, when using WordPress, we got 29 issues in total 
-        # (14 errors and 15 warnings) for "EditSummaryFragments.php")).
+        # That is, if we pass OverflowStyle (by GET or POST), we
+        # can turn off the WordPress part (e.g. to ease HTML
+        # validation (for example, when using WordPress, we
+        # got 29 issues in total (14 errors and 15 warnings)
+        # for "EditSummaryFragments.php")).
         #
         # Examples:
         #
@@ -53,6 +62,7 @@
         #
         #    <https://pmortensen.eu/world/EditSummaryFragments.php?OverflowStyle=Native>
         #
+        #    <https://pmortensen.eu/world/Text.php?OverflowStyle=Native>
 
 
         # The default is to use WordPress, if not explicitly turned off
@@ -76,13 +86,57 @@
     } #useWordPress()
 
 
+    # Single quotes, etc. are currently escaped if
+    # using WordPress (that is, in the form data).
+    #
+    # Note: Currently it is only done for a specific element,
+    #       "someText" (used by a specific page, Text.php)
+    #
+    function adjustForWordPressMadness()
+    {
+        global $_REQUEST;
+        #global MAINTEXT;
+        global $formDataSizeDiff;
+
+        # When we open the form (URL with ".php") there isn't
+        # form data.
+        if (array_key_exists(MAINTEXT, $_REQUEST))
+        {
+            $formDataSizeBefore = strlen($_REQUEST[MAINTEXT]);
+
+            #echo "<p>formDataSizeBefore: $formDataSizeBefore</p>\n";
+
+            # Only when WordPress is active (otherwise we get errors)
+            if (function_exists('stripslashes_deep'))
+            {
+                # Escape problem "fix" (ref. <https://stackoverflow.com/a/33604648>)
+                # The problem is solely due to WordPress (we would't need it
+                # if it wasn't for the use of/integration into WordPress).
+                #
+                # "stripslashes_deep" is part of WordPress
+                #
+                $_REQUEST = array_map('stripslashes_deep', $_REQUEST);
+            }
+
+            $formDataSizeAfter = strlen($_REQUEST[MAINTEXT]);
+
+            # Note: Only for one specific element,
+            #       "someText", only used in Text.php.
+            #
+            $formDataSizeDiff = $formDataSizeBefore - $formDataSizeAfter;
+
+            #echo "<p>formDataSizeDiff: $formDataSizeDiff</p>\n";
+        }
+    }
+
+
     # Single place for output of dynamic "value" attributes in HTML forms.
     function the_formValue($aRawContentlinkInlineMarkdown)
     {
-        # Later: Make it more robust - encode double 
-        #        quote (") as "&quot;". Probably 
+        # Later: Make it more robust - encode double
+        #        quote (") as "&quot;". Probably
         #        also single quote.
-    
+
         echo "value=\"$aRawContentlinkInlineMarkdown\"\n";
     }
 
@@ -96,34 +150,34 @@
 
     if (useWordPress())
     {
-	    # For getting the styling and other redundant
-	    # content (like headers) from WordPress.
-	    #
-	    # So now we have a dependency on WordPress...
-	    #
-	    define('WP_USE_THEMES', false);
-	    require(dirname(__FILE__) . '/wp-blog-header.php');
+        # For getting the styling and other redundant
+        # content (like headers) from WordPress.
+        #
+        # So now we have a dependency on WordPress...
+        #
+        define('WP_USE_THEMES', false);
+        require(dirname(__FILE__) . '/wp-blog-header.php');
 
-	    get_header(); # Note: Using some WordPress themes results in the following
-	                  #       on the page itself (though we also have it in the
-	                  #       title for all themes - but this is less intrusive):
-	                  #
-	                  #           "Page not found"
-	                  #
-	                  #       Some themes that do not give it are:
-	                  #
-	                  #           "Responsive"    (the one we currently use)
-	                  #           "Orfeo"
-	                  #           "Hestia"
-	                  #           "Astra"
+        get_header(); # Note: Using some WordPress themes results in the following
+                      #       on the page itself (though we also have it in the
+                      #       title for all themes - but this is less intrusive):
+                      #
+                      #           "Page not found"
+                      #
+                      #       Some themes that do not give it are:
+                      #
+                      #           "Responsive"    (the one we currently use)
+                      #           "Orfeo"
+                      #           "Hestia"
+                      #           "Astra"
     }
     else
     {
-    	# Revert to old-style header
-    	#
-    	# Yes, the Heredoc style makes it ugly.
-    	#
-    	echo <<<'HTML_END'
+        # Revert to old-style header
+        #
+        # Yes, the Heredoc style makes it ugly.
+        #
+        echo <<<'HTML_END'
 <!DOCTYPE html>
 <html lang="en">
 
@@ -133,7 +187,8 @@
         <title>Some edit summary fragments - Edit Overflow</title>
 
         <style>
-            body {
+            body
+            {
                 background-color: lightgrey;
             }
         </style>
@@ -214,10 +269,5 @@ HTML_END;
     } # End of native HTML part (not WordPress)
 
 ?>
-
-
-
-
-
 
 

Web_Application/EditSummaryFragments.php
@@ -1,14 +1,23 @@
 
-<!-- Successor to EditSummaryFragments.html, at least for now to get
-     the WordPress styling. But perhaps we can instead
+<!-- Successor to EditSummaryFragments.html, at least for now
+     to get the WordPress styling. But perhaps we can instead
      move this (effectively) static page into the
      WordPress system?
 
      Though we don't get things like a centralised place for
      the Edit Overflow version. Or other redundancy-reducing
      measures.
+-->
+
+<!--
+    Note:
+
+      We can now use "OverflowStyle=Native" to avoid the WordPress overhead:
+
+        <https://pmortensen.eu/world/EditSummaryFragments.php?OverflowStyle=Native>
 
 -->
+
 
 <?php include("commonStart.php"); ?>
 
@@ -503,7 +512,5 @@
 
 
 <?php include("commonEnd.php"); ?>
-
-
 
 

Web_Application/FixedStrings.php
@@ -5,6 +5,16 @@
      WordPress system?   -->
 
 
+<!--
+    Note:
+
+      We can now use "OverflowStyle=Native" to avoid the WordPress overhead:
+
+        <https://pmortensen.eu/world/FixedStrings.php?OverflowStyle=Native>
+
+-->
+
+
 <?php include("commonStart.php"); ?>
 
 

Web_Application/Text.php
@@ -1,21 +1,34 @@
-
 <?php
-     #    Strange things are happening when this WordPress thing is
-     #    included (a fix was added 2019-11-30, and finally
-     #    another one fix 2019-12-10).
+     # Strange things are happening when this WordPress thing
+     # is included (a fix was added 2019-11-30, and finally
+     # another one fix 2019-12-10).
      #
-     #      1. The YouTube convert (that uses a separate
-     #         PHP class in another file) had a lot of
-     #         warning lines. This turned out to be genuine
-     #         bug - the WordPress include had a configuration
-     #         that turned on debugging and surface the error.
+     #   1. The YouTube convert (that uses a separate
+     #      PHP class in another file) had a lot of
+     #      warning lines. This turned out to be genuine
+     #      bug - the WordPress include had a configuration
+     #      that turned on debugging and surface the error.
      #
-     #      2. Removing trailing space escapes single quotes
-     #         with backslash... This is a WordPress thing
-     #         (returned form data is escaped). The workaound
-     #         was to remove backslashes (this may be
-     #         sufficient as we don't )
+     #   2. Removing trailing space escapes single quotes
+     #      with backslash... This is a WordPress thing
+     #      (returned form data is escaped). The workaound
+     #      was to remove backslashes (this may be
+     #      sufficient as we don't )
 ?>
+
+
+<!--
+    Note:
+
+      We can now use "OverflowStyle=Native" to avoid the WordPress overhead:
+
+        <https://pmortensen.eu/world/Text.php?OverflowStyle=Native>
+
+        Using HTML GET parameter and invoking function "Format as keyboard":
+
+          <https://pmortensen.eu/world/Text.php?someText=dont&action=Format%20as%20keyboard&OverflowStyle=Native>
+
+-->
 
 
 <?php include("commonStart.php"); ?>
@@ -25,13 +38,10 @@
             the_EditOverflowHeadline("Text Stuff");
         ?>
 
-
-
         <?php
             require_once('StringReplacerWithRegex.php');
 
             # function assert()
-
 
             function removeTrailingSpace($aText)
             {
@@ -40,10 +50,12 @@
                 return preg_replace('/[ \t]+(\r?$)/m', '$1', $aText);
             }
 
+
             function replaceTABS_withSpace($aText)
             {
                 return preg_replace('/\t/', '    ', $aText);
             }
+
 
             function removeTrailingSpacesAndTABs($aText)
             {
@@ -73,6 +85,7 @@
                 }
                 return [$text2, $message2];
             }
+
 
             # Note: This (first) version only works well if
             #       there are actually some leading space
@@ -116,6 +129,7 @@
                 return $commonLeadingSpaces;
             }
 
+
             function removeCommonLeadingSpaces($aText, $aLeadingSpaces)
             {
                 $lines = explode("\n", $aText);
@@ -138,7 +152,6 @@
                 }
                 return implode("\n", $newContent);
             }
-
 
             # ----------------------- End of main functions ---------------------------
 
@@ -187,10 +200,13 @@
             # -------------------------------------------------------------
 
 
-            const MAINTEXT = 'someText';
+            #Delete at any time
+            #const MAINTEXT = 'someText';
+
 
             $message = "";
             $extraMessage = "";
+
 
             #assert(array_key_exists('function', $_REQUEST));  # From example:  isset($this->records)
             #
@@ -207,16 +223,18 @@
             $someText = "";
             if (array_key_exists(MAINTEXT, $_REQUEST))
             {
-                # Escape problem "fix" (ref. <https://stackoverflow.com/a/33604648>)
-                # The problem is solely due to WordPress (we would't need if
-                # it wasn't for the use of/integration into WordPress).
+
+                #Delete at any time
+                ## Escape problem "fix" (ref. <https://stackoverflow.com/a/33604648>)
+                ## The problem is solely due to WordPress (we would't need if
+                ## it wasn't for the use of/integration into WordPress).
+                ##
+                ## "stripslashes_deep" is part of WordPress
+                ##
+                #$formDataSizeBefore = strlen($_REQUEST[MAINTEXT]);
                 #
-                # "stripslashes_deep" is part of WordPress
-                #
-                $formDataSizeBefore = strlen($_REQUEST[MAINTEXT]);
-                $_REQUEST = array_map('stripslashes_deep', $_REQUEST);
-                $formDataSizeAfter = strlen($_REQUEST[MAINTEXT]);
-                $formDataSizeDiff = $formDataSizeBefore - $formDataSizeAfter;
+                #$_REQUEST = array_map('stripslashes_deep', $_REQUEST);
+
 
                 # Some output to remind us that this WordPress madness
                 # should be adressed some day
@@ -226,6 +244,7 @@
                       $formDataSizeDiff .
                       " characters saved from WordPress madness...";
                 }
+
 
                 $someText = htmlentities($_REQUEST[MAINTEXT]);
 
@@ -331,8 +350,8 @@
 
                         $leadingSpaceToRemove = findCommonLeadingSpaces($someText);
                         $someText = removeCommonLeadingSpaces($someText, $leadingSpaceToRemove);
-                        
-                        $message = "<p>Removed " . $leadingSpaceToRemove . 
+
+                        $message = "<p>Removed " . $leadingSpaceToRemove .
                                    " leading spaces from all lines...</p>\n";
                         break;
 

commit 990ae4cd875b7a72b0baf2c9b6d5c253b49e460f

Fixed regression from 976a80fcc9bffdab8300cea53f26f823c0eacbe4 (2019-12-18T200800Z+0): The WordPress madness correction for the main lookup (escaped single quotes, etc.) is now working again. It also gave false negative look up results, e.g. for "don't".

Web_Application/commonStart.php
@@ -19,8 +19,13 @@
     require_once('eFooter.php');
 
 
-    # Only used by one page (Text.php)
+    # Used by one page (EditOverflow.php)
+    const LOOKUPTERM = 'LookUpTerm';
+
+    # Used by another page (Text.php)
     const MAINTEXT = 'someText';
+
+
     $formDataSizeDiff = -1;
 
 
@@ -32,7 +37,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a5 2019-12-19T214706Z+0";
+        return "Edit Overflow v. 1.1.49a6 2019-12-27T222320Z+0";
     }
 
 
@@ -163,7 +168,10 @@
 
         # When we open the form (URL with ".php") there isn't
         # any form data.
-        if (array_key_exists(MAINTEXT, $_REQUEST))
+        if (
+            array_key_exists(LOOKUPTERM, $_REQUEST) ||  # Main - lookup
+            array_key_exists(MAINTEXT,   $_REQUEST)     # Textstuff page
+           )
         {
             $formDataSizeBefore = strlen(get_postParameter(MAINTEXT));
 
@@ -172,6 +180,8 @@
             # Only when WordPress is active (otherwise we get errors)
             if (function_exists('stripslashes_deep'))
             {
+                #echo "<p>stripslashes_deep() exists...</p>\n";
+
                 # Escape problem "fix" (ref. <https://stackoverflow.com/a/33604648>)
                 # The problem is solely due to WordPress (we would't need it
                 # if it wasn't for the use of/integration into WordPress).