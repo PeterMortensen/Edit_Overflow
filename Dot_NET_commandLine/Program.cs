507d2375c7120c438696febcde192e505f00f9ef
Peter Mortensen (XP64_NEW) <penge@pmortensen.eu>
2020-02-06 03:38:50 +0100

    For Linux: Added drivers, etc. so we check the wordlist data integrity immediately after a change to the word list.

---
+++ Dot_NET_commandLine/Program.cs
@@ -0,0 +1,42 @@
+﻿
+// First driver for using the .NET part Edit Overflow on Linix.
+//
+// The purpose is to run the word list integrity test
+// independent of Windows and Visual Studio.
+
+
+
+using System; // For Console
+
+using OverflowHelper.core;
+
+
+namespace EditOverflow2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            // This will result in running the first level of 
+            // integrity testing for the word list data
+            WikipediaLookup someWikipediaLookup = new WikipediaLookup();
+
+            // This will result in running more regorious
+            // integrity testing for the word list data
+            string Wordlist_HTML =
+              someWikipediaLookup.dumpWordList_asHTML(
+
+                // Fixed strings - sufficient for integrity testing
+                // of the word list data
+                //
+                "some combined regular expressions",
+                "some version thingie",
+                "some date only string");
+
+
+        }
+    }
+}
