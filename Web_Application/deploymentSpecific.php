ec647698170c9827023bd591963804b03f0fe417
Peter Mortensen (XP64_NEW) <penge@pmortensen.eu>
2020-04-22 00:29:41 +0200

    Refactored the PDO init process (and added an option for failure injection (e.g. for regression testing purposes)).

--- Web_Application/deploymentSpecific.php
+++ Web_Application/deploymentSpecific.php
@@ -11,20 +11,48 @@
     #
     function connectToDatabase()
     {
-	     # Note:
-	     #
-	     #  "charset=utf8" is required for e.g. lookup of "lets" to work -
-	     #  where 'correctTerm' contains the Quora quote character,
-	     #  "U+2019  E2 80 99  RIGHT SINGLE QUOTATION MARK".
-	     #
-	     #  If not, the resulting content of 'correctTerm' is
-	     #  "U+FFFD  EF BF BD  REPLACEMENT CHARACTER".
-	     #
-	     $somePDOinstance = new PDO(
-	       #'mysql:host=mysql19.unoeuro.com;dbname=pmortensen_eu_db',
-	       'mysql:host=mysql19.simply.com;dbname=pmortensen_eu_db;charset=utf8',
-	       'pmortensen_eu',
-	       '__pw_toBeFilledInDuringDeployment_'); # Sample key: 2019-09-16
+        $datebaseServer_instance = 'mysql19';
+
+        #'mysql:host=mysql19.unoeuro.com;dbname=pmortensen_eu_db',
+	    #'mysql:host=mysql19.simply.com;dbname=pmortensen_eu_db;charset=utf8',
+        #$datebaseServer_BaseDomain = 'unoeuro.com';
+        $datebaseServer_BaseDomain = 'simply.com';
+
+        $datebaseName = 'pmortensen_eu_db';
+
+
+        # For simulating failures (e.g. for password leaks and
+        # for regression testing)
+        $PDO_DoFail = get_postParameter('PDO_DoFail');
+        if ($PDO_DoFail === 'DNS') #Or simply take the domain as a parameter?
+        {
+            $datebaseServer_BaseDomain = 'simplyZZZZ.com';
+        }
+
+
+        # Sample:
+        #
+        #     mysql:host=mysql19.simply.com;dbname=pmortensen_eu_db;charset=utf8
+        #
+        $datebaseServer_address = $datebaseServer_instance . '.' .
+                                  $datebaseServer_BaseDomain;
+        $PDO_hostStr = 'mysql:host=' . $datebaseServer_address .
+                       ';dbname=' . $datebaseName . ';charset=utf8';
+
+        $pw = '__pw_toBeFilledInDuringDeployment_'; # Sample key: 2019-09-16
+
+
+
+	    # Note:
+	    #
+	    #  "charset=utf8" is required for e.g. lookup of "lets" to work -
+	    #  where 'correctTerm' contains the Quora quote character,
+	    #  "U+2019  E2 80 99  RIGHT SINGLE QUOTATION MARK".
+	    #
+	    #  If not, the resulting content of 'correctTerm' is
+	    #  "U+FFFD  EF BF BD  REPLACEMENT CHARACTER".
+	    #
+	    $somePDOinstance = new PDO($PDO_hostStr, 'pmortensen_eu', $pw);
 
         return $somePDOinstance;
     }
