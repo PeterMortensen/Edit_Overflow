0140067593d3e8d70db60762ee427817f81a486a
Peter Mortensen (XP64_NEW) <penge@pmortensen.eu>
2020-04-23 22:25:39 +0200

    Added the two new fules associated with the last revision...

---
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverfkowApplication_Unix.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System.Diagnostics; //For Trace. And its Assert.
+
+
+/****************************************************************************
+ *    <placeholder for header>                                              *
+ ****************************************************************************/
+namespace OverflowHelper
+{
+
+    // Why does it need to be public???
+    public struct clipboardInfoStruct
+    {
+        public int retries;
+    }
+
+
+    /****************************************************************************
+     *    "public" because of unit tests...                                     *
+     ****************************************************************************/
+    public class EditorOverflowApplication_Unix : EditorOverflowApplication
+    {
+
+
+        /****************************************************************************
+         *                                                                          *
+         *                                                                          *
+         ****************************************************************************/
+        public override void setClipboard3(
+            string aClipboardStr,
+            //System.Windows.Forms.ToolStripStatusLabel aStatusLabel
+            out string aStatusString
+            )
+        { 
+            aStatusString = "";
+        
+            Trace.Assert(false);
+            
+        } //setClipboard3()
+
+
+
+
+    } //class EditorOverflowApplication
+
+
+} //namespace OverflowHelper
+
+
+
