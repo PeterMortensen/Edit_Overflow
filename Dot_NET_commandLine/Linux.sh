64d64208840d360d58c6af05d0e94f7a97efbd0f
Peter Mortensen (XP64_NEW) <penge@pmortensen.eu>
2020-04-27 00:17:33 +0200

    Fixed a problem with false negatives for the filter for the word list integrity testing output (by using a more specific filter term) - but note that the fix creates a dependency on a name (that could change in the future) - it could be made more robust by a more complex filter (one that would match a comma separated list of words with a pair of parentheses). It is for filtering out the line "(incorrectTerm, correctTerm, URL)".

--- Dot_NET_commandLine/Linux.sh
+++ Dot_NET_commandLine/Linux.sh
@@ -121,7 +121,7 @@ grep INSERT $SQL_FILE | wc
 # word list data) out of the generated SQL or HTML (the two types
 # of output are currently mixed up...)
 echo
-grep -v DROP $SQL_FILE | grep -v pmortensen_eu_db | grep -v CREATE | grep -v VARCHAR | grep -v '^\#' | grep -v ')'  | grep -v '^$' | grep -v INSERT | grep -v '<tr>' | grep -v ' <' | grep -v '/>' | grep -v 'nbsp' | grep -v ';' | grep -v '2020-'
+grep -v DROP $SQL_FILE | grep -v pmortensen_eu_db | grep -v CREATE | grep -v VARCHAR | grep -v '^\#' | grep -v 'URL)'  | grep -v '^$' | grep -v INSERT | grep -v '<tr>' | grep -v ' <' | grep -v '/>' | grep -v 'nbsp' | grep -v ';' | grep -v '2020-'
 echo
 
 cd -
