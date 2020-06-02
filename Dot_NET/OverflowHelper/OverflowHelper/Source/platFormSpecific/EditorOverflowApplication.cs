057db10b4a9ad5b0b56503bfa24c2f9c55f57ea8
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2020-06-02 19:50:16 +0200

    Web: Make sure the next word list update (HTML) will have an updated date.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2020-05-10";
+            string toReturn = "2020-06-02";
             return toReturn;
         }
 
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a38"; 
+            string toReturn = "1.1.49a43"; 
             return toReturn;
         }
 
