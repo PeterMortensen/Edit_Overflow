666d2d8cc24ca1883ab238672789fae889ce3e8d
Peter Mortensen <penge@pmortensen.eu>
2021-05-15 17:58:24 +0200

    Web, HTML/PHP internal: Fixed inconsistent indentation, including added automatic enforcement of it in the build script (note: For some weird reasons most of indentation changes were not showm in the Visual Studio Code source control diff thingamajiggy).

--- Web_Application/FixedStrings.php
+++ Web_Application/FixedStrings.php
@@ -4,10 +4,11 @@
 ?>
 
 
-<!-- Successor to FixedStrings.html, at least for now to get
-     the WordPress styling. But perhaps we can instead
-     move this (effectively) static page into the
-     WordPress system?   -->
+<!--  Successor to FixedStrings.html, at least for now to get
+      the WordPress styling. But perhaps we can instead
+      move this (effectively) static page into the
+      WordPress system?
+-->
 
 
 <!--
@@ -39,206 +40,206 @@
 
             <p>Menu for<u>m</u>atting:
 
-               <input
-                   name="Menu formatting"
-                   type="text"
-                   id="MenuFormatting"
-                   class="XYZ29"
-                   value="* &amp;rarr; *"
-                   style="width:70px;"
-                   accesskey="M"
-                   title="Shortcut: Shift + Alt + M"
-               />
+                <input
+                    name="Menu formatting"
+                    type="text"
+                    id="MenuFormatting"
+                    class="XYZ29"
+                    value="* &amp;rarr; *"
+                    style="width:70px;"
+                    accesskey="M"
+                    title="Shortcut: Shift + Alt + M"
+                />
             </p>
 
             <p>HTML non-brea<u>k</u>ing space:
 
-               <input
-                   name="Non-breaking space"
-                   type="text"
-                   id="Non-breaking_space"
-                   class="XYZ32"
-                   value="&amp;nbsp;"
-                   style="width:50px;"
-                   accesskey="K"
-                   title="Shortcut: Shift + Alt + K"
-               />
+                <input
+                    name="Non-breaking space"
+                    type="text"
+                    id="Non-breaking_space"
+                    class="XYZ32"
+                    value="&amp;nbsp;"
+                    style="width:50px;"
+                    accesskey="K"
+                    title="Shortcut: Shift + Alt + K"
+                />
             </p>
 
             <p>HTML <u>b</u>reak:
 
-               <!-- Yes, leading space in 'value' -->
-               <input
-                   name="Break"
-                   type="text"
-                   id="Break"
-                   class="XYZ28"
-                   value=" <br/>"
-                   style="width:50px;"
-                   accesskey="B"
-                   title="Shortcut: Shift + Alt + B"
-               />
+                <!-- Yes, leading space in 'value' -->
+                <input
+                    name="Break"
+                    type="text"
+                    id="Break"
+                    class="XYZ28"
+                    value=" <br/>"
+                    style="width:50px;"
+                    accesskey="B"
+                    title="Shortcut: Shift + Alt + B"
+                />
             </p>
 
             <p>Back<u>t</u>ick:
 
-               <input
-                   name="Backtick"
-                   type="text"
-                   id="Backtick"
-                   class="XYZ39"
-                   value="`"
-                   style="width:50px;"
-                   accesskey="T"
-                   title="Shortcut: Shift + Alt + T"
-               />
+                <input
+                    name="Backtick"
+                    type="text"
+                    id="Backtick"
+                    class="XYZ39"
+                    value="`"
+                    style="width:50px;"
+                    accesskey="T"
+                    title="Shortcut: Shift + Alt + T"
+                />
             </p>
 
             <p>The <u>d</u>egrees symbol
-               (Unicode, e.g. for temperature):
+                (Unicode, e.g. for temperature):
 
-               <input
-                   name="Degrees symbol - Unicode"
-                   type="text"
-                   id="Degrees_Unicode"
-                   class="XYZ34"
-                   value="°"
-                   style="width:30px;"
-                   accesskey="D"
-                   title="Shortcut: Shift + Alt + D"
-               />
+                <input
+                    name="Degrees symbol - Unicode"
+                    type="text"
+                    id="Degrees_Unicode"
+                    class="XYZ34"
+                    value="°"
+                    style="width:30px;"
+                    accesskey="D"
+                    title="Shortcut: Shift + Alt + D"
+                />
             </p>
 
             <p>Arrow - &rarr; (<u>H</u>MTL):
 
-               <input
-                   name="Arrow - HMTL"
-                   type="text"
-                   id="Arrow_HMTL"
-                   class="XYZ30"
-                   value="&amp;rarr;"
-                   style="width:50px;"
-                   accesskey="H"
-                   title="Shortcut: Shift + Alt + H"
-               />
+                <input
+                    name="Arrow - HMTL"
+                    type="text"
+                    id="Arrow_HMTL"
+                    class="XYZ30"
+                    value="&amp;rarr;"
+                    style="width:50px;"
+                    accesskey="H"
+                    title="Shortcut: Shift + Alt + H"
+                />
             </p>
 
             <p><u>A</u>rrow (Unicode):
 
-               <input
-                   name="Arrow - Unicode"
-                   type="text"
-                   id="Arrow_Unicode"
-                   class="XYZ31"
-                   value="→"
-                   style="width:30px;"
-                   accesskey="A"
-                   title="Shortcut: Shift + Alt + A"
-               />
+                <input
+                    name="Arrow - Unicode"
+                    type="text"
+                    id="Arrow_Unicode"
+                    class="XYZ31"
+                    value="→"
+                    style="width:30px;"
+                    accesskey="A"
+                    title="Shortcut: Shift + Alt + A"
+                />
             </p>
 
             <p>Arro<u>w</u> (Unicode, alternative):
 
-               <input
-                   name="Arrow - Unicode - 2"
-                   type="text"
-                   id="Arrow_Unicode_2"
-                   class="XYZ40"
-                   value="►"
-                   style="width:30px;"
-                   accesskey=""
-                   title=""
-               />
+                <input
+                    name="Arrow - Unicode - 2"
+                    type="text"
+                    id="Arrow_Unicode_2"
+                    class="XYZ40"
+                    value="►"
+                    style="width:30px;"
+                    accesskey=""
+                    title=""
+                />
             </p>
 
             <p>The
-               <a href="https://en.wikipedia.org/wiki/Ohm"
-               ><u>o</u>hm symbol</a>
-               (Unicode, e.g. for
-               <a href="https://en.wikipedia.org/wiki/Electrical_resistance_and_conductance"
-               >electrical resistance</a>):
+                <a href="https://en.wikipedia.org/wiki/Ohm"
+                ><u>o</u>hm symbol</a>
+                (Unicode, e.g. for
+                <a href="https://en.wikipedia.org/wiki/Electrical_resistance_and_conductance"
+                >electrical resistance</a>):
 
-               <input
-                   name="Ohm symbol - Unicode"
-                   type="text"
-                   id="Ohm_Unicode"
-                   class="XYZ33"
-                   value="Ω"
-                   style="width:30px;"
-                   accesskey="O"
-                   title="Shortcut: Shift + Alt + O"
-               />
+                <input
+                    name="Ohm symbol - Unicode"
+                    type="text"
+                    id="Ohm_Unicode"
+                    class="XYZ33"
+                    value="Ω"
+                    style="width:30px;"
+                    accesskey="O"
+                    title="Shortcut: Shift + Alt + O"
+                />
             </p>
 
             <p>The mi<u>c</u>ro symbol
-               (Unicode, e.g. µs for microseconds):
+                (Unicode, e.g. µs for microseconds):
 
-               <input
-                   name="Greek micro symbol - Unicode"
-                   type="text"
-                   id="Micro_Unicode"
-                   class="XYZ35"
-                   value="µ"
-                   style="width:30px;"
-                   accesskey="C"
-                   title="Shortcut: Shift + Alt + C"
-               />
+                <input
+                    name="Greek micro symbol - Unicode"
+                    type="text"
+                    id="Micro_Unicode"
+                    class="XYZ35"
+                    value="µ"
+                    style="width:30px;"
+                    accesskey="C"
+                    title="Shortcut: Shift + Alt + C"
+                />
             </p>
 
             <p>True single <u>q</u>uote (not the ASCII one):
 
-               <input
-                   name="True single quote"
-                   type="text"
-                   id="TrueSingleQuote_Unicode"
-                   class="XYZ37"
-                   value="’"
-                   style="width:30px;"
-                   accesskey="Q"
-                   title="Shortcut: Shift + Alt + Q"
-               />
+                <input
+                    name="True single quote"
+                    type="text"
+                    id="TrueSingleQuote_Unicode"
+                    class="XYZ37"
+                    value="’"
+                    style="width:30px;"
+                    accesskey="Q"
+                    title="Shortcut: Shift + Alt + Q"
+                />
             </p>
 
             <p>True do<u>u</u>ble quotes (not the ASCII ones):
 
-               <input
-                   name="True double quote"
-                   type="text"
-                   id="TrueDoubleQuote_Unicode"
-                   class="XYZ38"
-                   value="“”"
-                   style="width:30px;"
-                   accesskey="U"
-                   title="Shortcut: Shift + Alt + U"
-               />
+                <input
+                    name="True double quote"
+                    type="text"
+                    id="TrueDoubleQuote_Unicode"
+                    class="XYZ38"
+                    value="“”"
+                    style="width:30px;"
+                    accesskey="U"
+                    title="Shortcut: Shift + Alt + U"
+                />
             </p>
 
             <p><a href="https://en.wikipedia.org/wiki/Tilde">Ti<u>l</u>de</a> (Unicode code point 007E):
 
-               <input
-                   name="Tilde"
-                   type="text"
-                   id="Tilde_Unicode"
-                   class="XYZ39"
-                   value="~"
-                   style="width:30px;"
-                   accesskey="L"
-                   title="Shortcut: Shift + Alt + L"
-               />
+                <input
+                    name="Tilde"
+                    type="text"
+                    id="Tilde_Unicode"
+                    class="XYZ39"
+                    value="~"
+                    style="width:30px;"
+                    accesskey="L"
+                    title="Shortcut: Shift + Alt + L"
+                />
             </p>
 
             <p><a href="https://en.wikipedia.org/wiki/Interpunct">Middle dot</a> - &middot; (Unicode code point 00B7):
 
-               <input
-                   name="middot"
-                   type="text"
-                   id="middot_Unicode"
-                   class="XYZ40"
-                   value="&amp;middot;"
-                   style="width:60px;"
-                   accesskey="X"
-                   title="Shortcut: Shift + Alt + X"
-               />
+                <input
+                    name="middot"
+                    type="text"
+                    id="middot_Unicode"
+                    class="XYZ40"
+                    value="&amp;middot;"
+                    style="width:60px;"
+                    accesskey="X"
+                    title="Shortcut: Shift + Alt + X"
+                />
             </p>
 
 
@@ -246,7 +247,7 @@
             <h2>Regular expressions for code checking</h2>
 
             <p>Note: As the false positive rate is high, every match
-                     should be checked manually</p>
+                      should be checked manually</p>
 
 
             <p>A<u>l</u>l combined
@@ -261,23 +262,23 @@
                   and
                   "Space before semicolon"):
 
-               <input
-                   name="All combined"
-                   type="text"
-                   id="codeRegex_AllCombined"
-                   class="XYZ36"
-                   value="(\S\{|:\S|,\S|\S\=|\=\S|\S\+|\+\S|\s,|\s:|\s\)|\s;|\(\s)"
-                   style="width:320px;"
-                   accesskey=""
-                   title=""
-               />
+                <input
+                    name="All combined"
+                    type="text"
+                    id="codeRegex_AllCombined"
+                    class="XYZ36"
+                    value="(\S\{|:\S|,\S|\S\=|\=\S|\S\+|\+\S|\s,|\s:|\s\)|\s;|\(\s)"
+                    style="width:320px;"
+                    accesskey=""
+                    title=""
+                />
             </p>
 
 
-            <!-- Submit button  - it ought to be invisible!
+            <!--  Submit button  - it ought to be invisible!
             -->
-            <!-- For 'value' (the displayed text in the button), tags 'u'
-                 or 'strong' do not work!! -->
+            <!--  For 'value' (the displayed text in the button), tags 'u'
+                  or 'strong' do not work!! -->
             <input
                 name="XYZ"
                 type="submit"
