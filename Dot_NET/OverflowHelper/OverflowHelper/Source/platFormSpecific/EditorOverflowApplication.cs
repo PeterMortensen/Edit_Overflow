d0eefd23fccf633efd1304ac2483b23952e1aa18
Peter Mortensen <penge@pmortensen.eu>
2021-08-19 12:56:31 +0200

    Version number update...

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2021-06-10";
+            string toReturn = "2021-08-19";
             return toReturn;
         }
 
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a97";
+            string toReturn = "1.1.49a99";
             return toReturn;
         }
 
