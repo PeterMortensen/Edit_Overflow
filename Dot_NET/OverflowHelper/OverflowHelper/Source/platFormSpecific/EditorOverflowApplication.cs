5c7e37964bd34122fe154378d33e071163a37eb1
Peter Mortensen <penge@pmortensen.eu>
2021-09-05 10:53:15 +0200

    Wordlist:  1) Corrected spelling of two correct words... 2) Changed a URL (to a Wikipedia one. The old one is also flagged by Firefox as "Did Not Connect: Potential Security Issue")

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a112";
+            string toReturn = "1.1.49a113";
             return toReturn;
         }
 
