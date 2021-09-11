076c492fbb4ba342b0ae1389f1a3fa6802ac470b
Peter Mortensen <penge@pmortensen.eu>
2021-09-11 13:00:19 +0200

    Wordlist: 1) Added more words. 2) Rearranged some word (starting with "web". Internal only - does not affect the actual word mappings).

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2021-09-10";
+            string toReturn = "2021-09-11";
             return toReturn;
         }
 
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a119";
+            string toReturn = "1.1.49a120";
             return toReturn;
         }
 
