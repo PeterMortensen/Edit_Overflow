822186288b5cfcfd63a40adcac4f617b59910f79
Peter Mortensen <penge@pmortensen.eu>
2021-11-04 22:32:06 +0100

    Web, text processing page: Added integration tests (Selenium) for the newly introduced conversion of Markdown four-space indent code.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a182";
+            string toReturn = "1.1.49a183";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -70,7 +70,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a182 2021-11-04T113748Z+0";
+        return "Edit Overflow v. 1.1.49a183 2021-11-04T134545Z+0";
     }
 
 
