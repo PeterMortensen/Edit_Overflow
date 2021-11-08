26823aaa2e6953feffd52ee7983dd95d7065ebd8
Peter Mortensen <penge@pmortensen.eu>
2021-11-09 00:38:15 +0100

    Wordlist export, HTML: Adding more escaping so it actually works (both for display and for the word lookup) for our new correct "word" "&lt;"...

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -70,7 +70,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a189 2021-11-08T195835Z+0";
+        return "Edit Overflow v. 1.1.49a190 2021-11-08T223919Z+0";
     }
 
 
