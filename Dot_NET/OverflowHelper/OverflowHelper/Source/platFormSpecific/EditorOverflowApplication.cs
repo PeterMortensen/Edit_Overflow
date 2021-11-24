4cb277e6075f367a866d932ae15904cae101ea8d
Peter Mortensen <penge@pmortensen.eu>
2021-11-24 14:22:07 +0100

    Wordlist: Added more words. And for real now: 1) Merged several mapping groups (essentially the same). 2) Changed some URLs to avoid redirects at Wikipedia or that were (effectively) broken. 3) Fixed some misspellings of words... (e.g., "Shlemiel") 4) Changed some words to use a real single quote

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2021-11-23";
+            string toReturn = "2021-11-24";
             return toReturn;
         }
 
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a211";
+            string toReturn = "1.1.49a212";
             return toReturn;
         }
 
