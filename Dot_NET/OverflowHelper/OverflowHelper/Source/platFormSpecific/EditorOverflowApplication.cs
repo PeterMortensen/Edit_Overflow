5635fa9d19f72add9e5c822128ae160019fb5130
Peter Mortensen <penge@pmortensen.eu>
2021-11-28 20:06:35 +0100

    Wordlist, URLs: 1) Wikipedia: Corrected an incorrect page and one that would result in a redirect at Wikipedia. 2) Wiktionary: Changed a word class and changed a page to point the expanded version

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a216";
+            string toReturn = "1.1.49a217";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -70,7 +70,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a216 2021-11-28T150906Z+0";
+        return "Edit Overflow v. 1.1.49a217 2021-11-28T182418Z+0";
     }
  
 
