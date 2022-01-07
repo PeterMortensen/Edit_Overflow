fc3766b5cdbc7630cea903fa18c787cdbe08741c
Peter Mortensen <penge@pmortensen.eu>
2022-01-07 19:31:56 +0100

    Wordlist: Added more words.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -70,7 +70,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a246 2022-01-06T203621Z+0";
+        return "Edit Overflow v. 1.1.49a247 2022-01-07T182027Z+0";
     }
 
 
