8c28246dae2d583c57d584cb346a691c79d3b93e
Peter Mortensen <penge@pmortensen.eu>
2022-01-11 19:24:51 +0100

    Wordlist: 1) All of the contained Stack Exchange sites can now be looked up by their (full official) name (as a result, some had to be moved to the alternative word set). 2) Merged some word mappings that were effectively identical (all Stack Exchange sites).

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -70,7 +70,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a250 2022-01-10T141522Z+0";
+        return "Edit Overflow v. 1.1.49a251 2022-01-11T181254Z+0";
     }
 
 
