732467c1f73c62ebf9a6a95683ebd2ac45fbe6ed
Peter Mortensen <penge@pmortensen.eu>
2022-01-13 16:42:38 +0100

    Wordlist: 1) Merged two word mappings. 2) Swapped a word mapping. 3) Fixed two (effectively) broken URLs. 4) Changed a word to use real single quotes. 5) Swapped a word in the alternative word set (based on the observed frequency).

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a254";
+            string toReturn = "1.1.49a255";
             return toReturn;
         }
 
