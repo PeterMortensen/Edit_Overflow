c5589b3250c240bb56cb16811e068a0174151b05
Peter Mortensen <penge@pmortensen.eu>
2022-02-01 18:05:34 +0100

    Wordlist: 1) Added more words. 2) Removed some word mappings that could conflict with newer mappings (and whose frequency is expected to decrease over time).

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -70,7 +70,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a275 2022-02-01T001056Z+0";
+        return "Edit Overflow v. 1.1.49a276 2022-02-01T165106Z+0";
     }
 
 
