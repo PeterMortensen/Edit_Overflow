94a2a666fac5596a1529283571a82b16e80cf9e0
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2022-04-29 12:30:14 +0200

    Wordlist: 1) Added more words. 2) Added some annotation about the alternative word list.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -73,7 +73,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a358 2022-04-28T130250Z+0";
+        return "Edit Overflow v. 1.1.49a359 2022-04-29T101743Z+0";
     }
 
 
