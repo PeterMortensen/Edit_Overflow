d1d60d3d2ee38e400d37cfe0ec48dba9ef98dbe5
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2022-05-18 15:50:01 +0200

    Wordlist: Added more words.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2022-05-17";
+            string toReturn = "2022-05-18";
             return toReturn;
         }
  
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a379";
+            string toReturn = "1.1.49a380";
             return toReturn;
         }
 
