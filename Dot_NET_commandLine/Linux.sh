ba66a44983c5929c1db32f9d77431d15355a67aa
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2022-07-14 14:10:44 +0200

    Build script: Updated a file size range check to reflect the growing word list

--- Dot_NET_commandLine/Linux.sh
+++ Dot_NET_commandLine/Linux.sh
@@ -2463,7 +2463,7 @@ cat '/home/mortensen/temp2/2022-02-25/Backup/Backup_2022-02-25_smallFiles/2022-0
 #       2022-01-25: 5374604 bytes
 
 # Note: The same build number
-wordListExport 31 "SQL" $SQL_FILE 5750000 6330000
+wordListExport 31 "SQL" $SQL_FILE 5870000 6460000
 
 # Note: The same build number
 export MATCHING_LINES=`grep -c 'DROP TABLE EditOverflow'  ${SQL_FILE}`
