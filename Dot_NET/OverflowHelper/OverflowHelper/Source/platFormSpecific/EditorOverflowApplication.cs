6b81c57279321ef98b6c15108ed4c220eb7b2107
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2022-08-07 14:34:12 +0200

    Wordlist: 1) Added more words. 2) Fixed an incorrect (in a specific context) URL. 3) Added an exception to one of the rules for URLs.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2022-08-06";
+            string toReturn = "2022-08-07";
             return toReturn;
         }
 
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a446";
+            string toReturn = "1.1.49a447";
             return toReturn;
         }
 
