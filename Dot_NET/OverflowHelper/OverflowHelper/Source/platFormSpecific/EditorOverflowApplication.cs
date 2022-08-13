f7193468f0df8eb726791b2cb6e7d36e3c3b8d29
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2022-08-13 13:47:16 +0200

    Wordlist: 1) Added more words. 2) Moved a correct word to the alternative word set to make space for a new mapping.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2022-08-12";
+            string toReturn = "2022-08-13";
             return toReturn;
         }
 
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a452";
+            string toReturn = "1.1.49a453";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -73,7 +73,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a452 2022-08-12T121140Z+0";
+        return "Edit Overflow v. 1.1.49a453 2022-08-13T114044Z+0";
     }
 
 
