1faeda1df13e649c9dd262e646b70054bdd84c9e
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2022-08-28 14:05:46 +0200

    Wordlist: Added more words.

--- Dot_NET/Tests/EnvironmentTests.cs
+++ Dot_NET/Tests/EnvironmentTests.cs
@@ -56,7 +56,7 @@ namespace OverflowHelper.Tests
             // Ensure we are actually running the expected version. But what
             // is actually our intent here? Why do we have this test?
 
-            string presumedNewest = "1.1.49a467";
+            string presumedNewest = "1.1.49a468";
  
             {
                 // For the main application
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -73,7 +73,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a467 2022-08-27T122745Z+0";
+        return "Edit Overflow v. 1.1.49a468 2022-08-28T115554Z+0";
     }
 
 
