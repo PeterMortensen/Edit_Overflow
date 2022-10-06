63dcadbd815d37ea8592591bd27087c1478cb95e
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2022-10-06 18:07:50 +0200

    Wordlist: 1) All Stack Overflow tag wiki mappings now also have the raw tag name as an incorrect word. 2) Added more words.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2022-10-05";
+            string toReturn = "2022-10-06";
             return toReturn;
         }
 
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a509";
+            string toReturn = "1.1.49a510";
             return toReturn;
         }
 
