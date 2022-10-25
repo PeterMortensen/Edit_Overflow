c630a3863fa03ab12dcb4df9f5401e2f36f7fc46
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2022-10-25 17:33:38 +0200

    Wordlist: 1) Added more words. 2) Improved the cross linking to the tag wiki entries.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a526";
+            string toReturn = "1.1.49a527";
             return toReturn;
         }
 
