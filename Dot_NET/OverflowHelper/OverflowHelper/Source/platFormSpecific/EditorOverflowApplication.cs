8d93fc0363426163239eaa25f6e66fffedbbf360
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2023-01-20 17:52:55 +0100

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2023-01-19";
+            string toReturn = "2023-01-20";
             return toReturn;
         }
 
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a620";
+            string toReturn = "1.1.49a621";
             return toReturn;
         }
 
