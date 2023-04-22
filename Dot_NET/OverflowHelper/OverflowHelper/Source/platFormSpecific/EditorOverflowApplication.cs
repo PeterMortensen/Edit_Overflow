cadfbddadc67d81c5bdbcf49139c33b7e383aa62
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2023-04-22 02:43:57 +0200

    Wordlist: 1) Added some line-break protection. 2) Merged some entries. 3) Corrected some correct words... 4) Split a mapping (was incorrect). 5) Added more words. 6) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a730";
+            string toReturn = "1.1.49a731";
             return toReturn;
         }
 
