b8d32075cfd5042a607dc2ef62f550a6da980723
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2023-07-11 18:25:01 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/Tests/EnvironmentTests.cs
+++ Dot_NET/Tests/EnvironmentTests.cs
@@ -56,7 +56,7 @@ namespace OverflowHelper.Tests
             // Ensure we are actually running the expected version. But what
             // is actually our intent here? Why do we have this test?
 
-            string presumedNewest = "1.1.49a827";
+            string presumedNewest = "1.1.49a828";
 
             {
                 // For the main application
