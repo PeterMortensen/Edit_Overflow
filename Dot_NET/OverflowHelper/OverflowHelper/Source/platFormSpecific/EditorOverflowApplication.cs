91829000bc54b76871f9305e80f092af753a0135
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2023-07-11 02:23:49 +0200

    Word list functionality: Refactoring: Consolidated three fields related to correct terms into one (struct)

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a824";
+            string toReturn = "1.1.49a825";
             return toReturn;
         }
 
