d2a88d6e4f74023fd842c9bfba6db170298fa088
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2023-08-08 15:00:39 +0200

    Wordlist: 1) Used the cross referencing between the word sets to cover most of common homonyms/homophones, like "than" vs. "then", "you're" vs. "your", "there" vs. "their" vs. "they're", "affect" vs. "effect", and "where" vs. "were"vs. "we're". 2) Added more words. 3) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2023-08-07";
+            string toReturn = "2023-08-08";
             return toReturn;
         }
 
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a855";
+            string toReturn = "1.1.49a856";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a855 2023-08-07T154353Z+0";
+        return "Edit Overflow v. 1.1.49a856 2023-08-08T125156Z+0";
     }
 
 
