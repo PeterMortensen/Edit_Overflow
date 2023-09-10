8c8eda598f546c3e0709a2c5889a53cfdb774664
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2023-09-10 18:40:28 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would. 3) To make space for a new mapping: Moved mappings between the two word sets and deleted some mappings.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2023-09-09";
+            string toReturn = "2023-09-10";
             return toReturn;
         }
 
@@ -96,7 +96,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a888";
+            string toReturn = "1.1.49a889";
             return toReturn;
         }
 
