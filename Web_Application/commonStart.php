3b9d612c001f59e4bff84912158b75bb452a5418
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2023-09-30 16:25:49 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would. 3) Fixed the spelling of a correct term... 4) Moved a (correct) word to the alternative word set to make space for a new mapping.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a910 2023-09-29T201630Z+0";
+        return "Edit Overflow v. 1.1.49a911 2023-09-30T141309Z+0";
     }
 
 
