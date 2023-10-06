8a33093509307e6a79a346314666cef5aa196893
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2023-10-06 05:04:40 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a915 2023-10-05T031826Z+0";
+        return "Edit Overflow v. 1.1.49a916 2023-10-06T025700Z+0";
     }
 
 
