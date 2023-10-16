4e3a3aa265226cf0607055f81e03d4a1785f3f16
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2023-10-16 21:46:40 +0200

    Wordlist: 1) Physically rearranged some items in the source (no actual change to the word list). A normal diff may not make much sense, only comparing them in a sorted form. 2) Added some annotation (no code changes). [2]

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.49a929";
+            string toReturn = "1.1.49a930";
             return toReturn;
         }
 
