eb41020c3d6d07e81c598d9a56818eb5a647ee7e
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-01-02 14:41:10 +0100

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/Tests/EnvironmentTests.cs
+++ Dot_NET/Tests/EnvironmentTests.cs
@@ -56,7 +56,7 @@ namespace OverflowHelper.Tests
             // Ensure we are actually running the expected version. But what
             // is actually our intent here? Why do we have this test?
 
-            string presumedNewest = "1.1.49a998";
+            string presumedNewest = "1.1.49a999";
 
             {
                 // For the main application
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.49a998 2024-01-01T120515Z+0";
+        return "Edit Overflow v. 1.1.49a999 2024-01-02T133352Z+0";
     }
 
 
