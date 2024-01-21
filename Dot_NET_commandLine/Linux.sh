6b714b4128730896f840b49ddd47351888486b4c
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-01-21 03:22:29 +0100

    Build script: Increased some file size limit check values to reflect the growing word list

--- Dot_NET_commandLine/Linux.sh
+++ Dot_NET_commandLine/Linux.sh
@@ -2591,7 +2591,7 @@ cat '/home/mortensen/UserProf/At_PC2016/_Incorporated_files/Header_EditOverflow_
 #       2022-01-25: 5374604 bytes
 
 # Note: The same build number
-wordListExport 31 "SQL" $SQL_FILE 8870000 9750000
+wordListExport 31 "SQL" $SQL_FILE 9040000 9940000
 
 # Note: The same build number
 export MATCHING_LINES=`grep -c 'DROP TABLE EditOverflow'  ${SQL_FILE}`
