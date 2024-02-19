3bdfeff662dcab18aed320f8e577d40e6722f590
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-02-19 04:43:17 +0100

    Wordlist: Fixed some URLs, including for avoiding redirects at Wikipedia.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a148 2024-02-18T152204Z+0";
+        return "Edit Overflow v. 1.1.50a148 2024-02-19T032348Z+0";
     }
 
 
