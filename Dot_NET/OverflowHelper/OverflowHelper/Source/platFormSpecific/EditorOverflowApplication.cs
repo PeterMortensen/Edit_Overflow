572a39fa249f557ef8756e94e6b3f054792eecf4
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-06-03 16:10:11 +0200

    Wordlist: Round 2 - 1) Added words that previous could not be represented in two word lists due to ***conflicts*** with other other words (enabled by the now three word lists). This included moving words between word lists. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a289";
+            string toReturn = "1.1.50a290";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a289 2024-06-03T111432Z+0";
+        return "Edit Overflow v. 1.1.50a290 2024-06-03T140241Z+0";
     }
 
 
