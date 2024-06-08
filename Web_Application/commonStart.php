14b64b5fe018ad6f707719ed69dcc182d4b483ce
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-06-08 21:32:59 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would. 3) Converted some URLs to HTTPS.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a306 2024-06-07T235856Z+0";
+        return "Edit Overflow v. 1.1.50a307 2024-06-08T192111Z+0";
     }
 
 
