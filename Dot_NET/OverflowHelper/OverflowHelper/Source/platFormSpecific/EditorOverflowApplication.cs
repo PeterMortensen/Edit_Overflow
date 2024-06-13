86fc7e0805888a5f369b9112206bf9b054eb2eff
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-06-13 02:50:23 +0200

    Added a fourth word set (this is mostly (and still) by convention).

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a316";
+            string toReturn = "1.1.50a317";
             return toReturn;
         }
 
