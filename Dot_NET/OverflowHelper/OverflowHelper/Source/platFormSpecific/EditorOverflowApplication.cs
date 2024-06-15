b5981949889adfc0b8dc73339d71ddd00d10e1f9
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-06-15 03:08:12 +0200

    Web interface: 1) Lookup result: If a correct word was looked up, this is now indicated on the result page.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a329";
+            string toReturn = "1.1.50a330";
             return toReturn;
         }
 
