38ffa5a03e7717bdf44d118c0c5186d3f91a9c53
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-06-25 19:45:58 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would. 3) Corrected information about the K10 Pro.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a355 2024-06-24T205042Z+0";
+        return "Edit Overflow v. 1.1.50a356 2024-06-25T173139Z+0";
     }
 
 
