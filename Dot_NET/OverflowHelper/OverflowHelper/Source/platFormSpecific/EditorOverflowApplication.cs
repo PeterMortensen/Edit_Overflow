e8a87d5a51e08f19075a191777fd729c6900bf43
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-07-16 18:40:42 +0200

    Word list: Fixed some URLs (that were directly or effectively broken or half-broken)

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a400";
+            string toReturn = "1.1.50a401";
             return toReturn;
         }
 
