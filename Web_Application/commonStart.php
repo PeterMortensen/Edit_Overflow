20dca7c4674abde1bf32cb4f65bd2ec9c95c8249
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-07-16 18:40:42 +0200

    Word list: Fixed some URLs (that were directly or effectively broken or half-broken)

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a400 2024-07-16T153248Z+0";
+        return "Edit Overflow v. 1.1.50a401 2024-07-16T163334Z+0";
     }
 
 
