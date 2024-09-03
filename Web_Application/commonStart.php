0065e04b1ce16903ca703c67224379b14b94382e
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-09-03 22:10:00 +0200

    Wordlist: 1) Merged several mapping groups (essentially the same). 2) Changed some URLs to avoid redirects at Wikipedia or that were (effectively) broken. 3) Fixed some misspellings of words... (e.g., "Shlemiel") 4) Changed some words to use a real single quote

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a501 2024-09-03T112516Z+0";
+        return "Edit Overflow v. 1.1.50a502 2024-09-03T213834Z+0";
     }
 
 
