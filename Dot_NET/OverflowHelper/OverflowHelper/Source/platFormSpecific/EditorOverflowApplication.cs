2829c00c6a9c7e8f1bac55c0c24d0efa50c37485
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-09-09 22:45:32 +0200

    Wordlist: Physically rearranged some mechanical keyboard entries and changed the annotation for some of them - for part of it, a normal diff would not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a520";
+            string toReturn = "1.1.50a521";
             return toReturn;
         }
 
