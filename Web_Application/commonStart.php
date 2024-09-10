227007076928f4444297d84d56601bb418c39f07
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-09-10 17:22:40 +0200

    Wordlist: 1) Physically rearranged some mechanical keyboard entries and changed the annotation for some of them - for part of it, a normal diff would not make much sense; only comparing the two revisions of the file in a sorted form would. 2) Added more words. 3) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a523 2024-09-10T135133Z+0";
+        return "Edit Overflow v. 1.1.50a524 2024-09-10T151638Z+0";
     }
 
 
