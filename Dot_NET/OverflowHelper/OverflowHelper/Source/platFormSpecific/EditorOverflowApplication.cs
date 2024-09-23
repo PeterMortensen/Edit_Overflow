dec7037e4b1e16ec558c7e7e4c7014f0319cd22c
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-09-23 22:25:39 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a554";
+            string toReturn = "1.1.50a555";
             return toReturn;
         }
 
--- Dot_NET/Tests/EnvironmentTests.cs
+++ Dot_NET/Tests/EnvironmentTests.cs
@@ -56,7 +56,7 @@ namespace OverflowHelper.Tests
             // Ensure we are actually running the expected version. But what
             // is actually our intent here? Why do we have this test?
 
-            string presumedNewest = "1.1.50a554";
+            string presumedNewest = "1.1.50a555";
 
             {
                 // For the main application
