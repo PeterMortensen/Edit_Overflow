068fe5503edfd556ac99f6e1b26034d0bba7b4af
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-09-30 03:57:04 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a564";
+            string toReturn = "1.1.50a565";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a564 2024-09-29T162313Z+0";
+        return "Edit Overflow v. 1.1.50a565 2024-09-30T014004Z+0";
     }
 
 
