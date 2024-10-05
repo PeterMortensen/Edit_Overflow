3fc066bafbfe4d823d8fb3d55a437667cef7c9b2
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-10-05 18:05:55 +0200

    Wordlist: Keyboard percentages are now consistent with the number of keys, to the nearest 5%, etc.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a576 2024-10-05T030229Z+0";
+        return "Edit Overflow v. 1.1.50a577 2024-10-05T155748Z+0";
     }
 
 
