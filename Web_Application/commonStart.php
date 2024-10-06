b7b27297cc0a810883c194b6a8859ef3bf7e8faf
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-10-06 21:33:43 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would. 3. Made the keyboard information more regular, incl. a percentage (rounding to nearest 5%) for all entries.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a579 2024-10-06T140353Z+0";
+        return "Edit Overflow v. 1.1.50a580 2024-10-06T191811Z+0";
     }
 
 
