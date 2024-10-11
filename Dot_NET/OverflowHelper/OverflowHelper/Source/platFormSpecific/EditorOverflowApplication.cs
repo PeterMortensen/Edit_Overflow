9fe4cc0601b027d62e31b0a015d9623735e8bb81
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-10-11 23:08:04 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would. 3) The QMK keycode entries are now more regular: Ordered by raw keycode and all contain a mapping with the full keycode. 4) Added mappings for the new names for mouse scroll QMK keycodes (both full name and alias).

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a591";
+            string toReturn = "1.1.50a592";
             return toReturn;
         }
 
