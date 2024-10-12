6649cf02a9eef434b3c6c0158dfb622f740113c2
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-10-13 01:56:57 +0200

    Wordlist: 1) Added more words. 2) Added some more meta information to the Keychron K and K Max series keyboards.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a594";
+            string toReturn = "1.1.50a595";
             return toReturn;
         }
 
