bcde95637dc39239db13075dbc0a1bad3eca3ef8
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-10-21 15:49:44 +0200

    Wordlist: 1) Added annotation for some of the mechanical keyboard entries. 2) Some of the annotation for the mechanical keyboard entries is now more regular.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a620 2024-10-21T125119Z+0";
+        return "Edit Overflow v. 1.1.50a621 2024-10-21T133816Z+0";
     }
 
 
