17e80f808c580c68143f9201ffdd614d3f780e0a
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-10-28 05:15:13 +0100

    Added package.json back in...

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a639";
+            string toReturn = "1.1.50a640";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a639 2024-10-28T025419Z+0";
+        return "Edit Overflow v. 1.1.50a640 2024-10-28T041421Z+0";
     }
 
 
