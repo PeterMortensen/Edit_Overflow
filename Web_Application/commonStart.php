79c16ad6cd8b21af1c527b3fb5f3373098a0aba1
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-11-01 02:11:18 +0100

    Wordlist: 1) Added more words related to URL encoding, including corresponding entries for QMK key codes and ASCII codes. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a649 2024-10-31T154653Z+0";
+        return "Edit Overflow v. 1.1.50a650 2024-11-01T005354Z+0";
     }
 
 
