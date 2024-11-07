ee93e34bebb2f28ecb5af907141f5a454bb0e868
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-11-07 23:10:34 +0100

    1) Added cross references for some QMK entries. Wordlist: 2) Added more words. 3) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a666";
+            string toReturn = "1.1.50a667";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a666 2024-11-07T201203Z+0";
+        return "Edit Overflow v. 1.1.50a667 2024-11-07T215248Z+0";
     }
 
 
