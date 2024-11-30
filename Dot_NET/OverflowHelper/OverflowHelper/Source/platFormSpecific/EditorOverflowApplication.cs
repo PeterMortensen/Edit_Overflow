e31004aa0e1dcb00535a4d3758b81bf3168d17d8
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-11-30 16:31:52 +0100

    Wordlist: 1) Fixed an incorrect URL. 2) Added more words.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a697";
+            string toReturn = "1.1.50a698";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a697 2024-11-30T123030Z+0";
+        return "Edit Overflow v. 1.1.50a698 2024-11-30T152431Z+0";
     }
 
 
