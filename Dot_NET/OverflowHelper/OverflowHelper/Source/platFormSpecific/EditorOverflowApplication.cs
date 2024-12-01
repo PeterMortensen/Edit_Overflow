7c4beeba4ae99c4ebc704bc5f085c8d1336c4d70
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-12-01 21:31:34 +0100

    Refactored the main word set into a separate function, like for the other word sets (for example, this makes it easier to selectively enable and disable word sets for testing purposes).

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a703";
+            string toReturn = "1.1.50a704";
             return toReturn;
         }
 
