24c232e9381f5a5f3239546a554fe557b9751f7f
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2024-12-29 01:59:43 +0100

    Wordlist: 1) Added many entries for PowerShell aliases. 2) Added more words. 3) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a734";
+            string toReturn = "1.1.50a735";
             return toReturn;
         }
 
