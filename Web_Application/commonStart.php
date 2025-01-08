851588c7600242573da0956488b8d30ad09e726f
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-01-08 01:55:38 +0100

    Wordlist: 1. Introduced cross references to source code for Keychron keyboards. 2) Added more words. 3) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a764 2025-01-07T215931Z+0";
+        return "Edit Overflow v. 1.1.50a765 2025-01-08T004949Z+0";
     }
 
 
