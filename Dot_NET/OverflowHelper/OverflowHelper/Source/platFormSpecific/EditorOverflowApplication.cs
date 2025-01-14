3fbd41bcb49e65e3a565faacd41d0692520f56f4
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-01-14 18:33:57 +0100

    Wordlist: 1) Moved entries between wordsets to get mapping "ad" for "advertisement" to the main word set. 2) Added more words. 3) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2025-01-13"; // Note: Having the exact length
+            string toReturn = "2025-01-14"; // Note: Having the exact length
                                             //       of 10 will be caught in
                                             //       one of the units tests...
             return toReturn;
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a782";
+            string toReturn = "1.1.50a783";
             return toReturn;
         }
 
