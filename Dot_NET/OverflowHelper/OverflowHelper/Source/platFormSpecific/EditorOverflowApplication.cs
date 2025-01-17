024315759028ef0d3994bb9f2093bd6ed5a9c4d1
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-01-17 03:39:09 +0100

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a788";
+            string toReturn = "1.1.50a789";
             return toReturn;
         }
 
