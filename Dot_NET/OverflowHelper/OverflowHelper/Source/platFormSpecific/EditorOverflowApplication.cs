5ebd50d56face2dff00c8a4bbbc254dc3e04243b
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-01-29 14:03:10 +0100

    .NET, lookup: Refactoring related to automatic stripping of some trailing and leading characters (no change in functionality)

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a825";
+            string toReturn = "1.1.50a826";
             return toReturn;
         }
 
