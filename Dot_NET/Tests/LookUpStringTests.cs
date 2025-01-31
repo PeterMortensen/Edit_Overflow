c2fefda8a0e9bd231495a5515eb2d3b8e8099cdc
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-01-31 02:11:19 +0100

    .NET, lookup: Some refactoring related to automatic stripping of trailing and leading characters (no change in functionality)

--- Dot_NET/Tests/LookUpStringTests.cs
+++ Dot_NET/Tests/LookUpStringTests.cs
@@ -58,7 +58,12 @@ namespace LookUpStringTests
 
 
         /****************************************************************************
-         *    <placeholder for header>                                              *
+         *                                                                          *
+         *  Test of our automatic filtering out of leading and                      *
+         *  trailing space, punctuation, Markdown formatting,                       *
+         *  etc.                                                                    *
+         *                                                                          *
+         *                                                                          *
          ****************************************************************************/
         [Test]
         public void leadingAndTrailingWhiteSpace()
@@ -155,6 +160,72 @@ namespace LookUpStringTests
 
                 string cs = tt2.getCoreString();
                 Assert.AreEqual("*", cs, "");
+
+                string leading = tt2.getLeading();
+                Assert.AreEqual("", leading, "");
+
+                string trailing = tt2.getTrailing();
+                Assert.AreEqual("", trailing, "");
+            }
+
+            // Leading double quote. From a user perspective,
+            // it shouldn't be filtered out (as it is a valid
+            // word to look up), but that is the current
+            // behaviour. The workaround is to turn off
+            // the automatic filtering.
+            {
+                // A valid word to look up (including the quotes):
+                //
+                //   "Timeless" Homerow
+                //
+                LookUpString tt2 =
+                    new LookUpString("\"Timeless\" Homerow");
+                string cs = tt2.getCoreString();
+                Assert.AreEqual("Timeless\" Homerow", cs, "");
+
+                string leading = tt2.getLeading();
+                Assert.AreEqual("\"", leading, "");
+
+                string trailing = tt2.getTrailing();
+                Assert.AreEqual("", trailing, "");
+            }
+
+            // Test of the option to turn automatic
+            // filtering (mostly) off: Space
+            {
+                LookUpString tt2 = new LookUpString(" OS ", false);
+                string cs = tt2.getCoreString();
+                Assert.AreEqual(" OS ", cs, "");
+
+                string leading = tt2.getLeading();
+                Assert.AreEqual("", leading, "");
+
+                string trailing = tt2.getTrailing();
+                Assert.AreEqual("", trailing, "");
+            }
+
+            // Test of the option to turn automatic
+            // filtering (mostly) off: Leading double quote
+            {
+                LookUpString tt2 =
+                    new LookUpString("\"Timeless\" Homerow", false);
+                string cs = tt2.getCoreString();
+                Assert.AreEqual("\"Timeless\" Homerow", cs, "");
+
+                string leading = tt2.getLeading();
+                Assert.AreEqual("", leading, "");
+
+                string trailing = tt2.getTrailing();
+                Assert.AreEqual("", trailing, "");
+            }
+
+            // Test of the option to turn automatic
+            // filtering (mostly) off: Trailing double quote
+            {
+                LookUpString tt2 =
+                    new LookUpString("Debian 10 \"Buster\"", false);
+                string cs = tt2.getCoreString();
+                Assert.AreEqual("Debian 10 \"Buster\"", cs, "");
 
                 string leading = tt2.getLeading();
                 Assert.AreEqual("", leading, "");
