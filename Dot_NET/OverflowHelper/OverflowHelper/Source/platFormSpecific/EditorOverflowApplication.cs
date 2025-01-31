0ee0830bb833c64cf707c00073e125300ce3caed
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-01-31 23:37:19 +0100

    Wordlist: Renamed some words (expansion)

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a842";
+            string toReturn = "1.1.50a843";
             return toReturn;
         }
 
