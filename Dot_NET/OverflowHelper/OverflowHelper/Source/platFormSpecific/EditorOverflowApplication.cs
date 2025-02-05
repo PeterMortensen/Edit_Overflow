6d7bd76da6c9297a42fe453a0e1029f99298326d
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-02-05 04:03:36 +0100

    Wordlist: 1) Removed a newly added mapping. 2) Added more words.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a855";
+            string toReturn = "1.1.50a856";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a855 2025-02-05T022837Z+0";
+        return "Edit Overflow v. 1.1.50a856 2025-02-05T025753Z+0";
     }
 
 
