a1b82b0fef0dfdfe0b19b56e82b51982a31537b2
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-02-12 21:10:26 +0100

    Wordlist: 1) Added some QMK keycodes. 2) Added more words.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a861";
+            string toReturn = "1.1.50a862";
             return toReturn;
         }
 
