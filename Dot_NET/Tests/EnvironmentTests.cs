bd7d884b809284f45d5a47fa399e715843d734b2
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-02-12 18:08:40 +0100

    Wordlist: 1) Reassigned a falsely assigned bad word. 2) Added more words.

--- Dot_NET/Tests/EnvironmentTests.cs
+++ Dot_NET/Tests/EnvironmentTests.cs
@@ -56,7 +56,7 @@ namespace OverflowHelper.Tests
             // Ensure we are actually running the expected version. But what
             // is actually our intent here? Why do we have this test?
 
-            string presumedNewest = "1.1.50a860";
+            string presumedNewest = "1.1.50a861";
 
             {
                 // For the main application
