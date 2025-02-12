454a20f2a235f2db3f44e14fd486d9e3fb99034c
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-02-12 18:08:40 +0100

    Wordlist: 1) Reassigned a falsely assigned bad word. 2) Added more words.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -86,7 +86,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public string versionString_dateOnly()
         {
-            string toReturn = "2025-02-11"; // Note: Having the exact length
+            string toReturn = "2025-02-12"; // Note: Having the exact length
                                             //       of 10 will be caught in
                                             //       one of the units tests...
             return toReturn;
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a860";
+            string toReturn = "1.1.50a861";
             return toReturn;
         }
 
