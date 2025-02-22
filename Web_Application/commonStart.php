a82527293a1a0888884997d6517af19b5ad6b70b
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-02-22 15:12:33 +0100

    Wordlist: 1) Made the QMK keycode entries more consistent. 2) Added more words.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a879 2025-02-22T000138Z+0";
+        return "Edit Overflow v. 1.1.50a880 2025-02-22T140131Z+0";
     }
 
 
