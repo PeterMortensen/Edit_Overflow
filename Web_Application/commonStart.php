47fbcefd7d33b434713862a86e9a0d204eea64c6
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-03-05 19:14:45 +0100

    Wordlist: 1) Moved entries between wordsets to make space for new mappings. 2) Added more words. 3) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a912 2025-03-04T143049Z+0";
+        return "Edit Overflow v. 1.1.50a913 2025-03-05T161912Z+0";
     }
 
 
