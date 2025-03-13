55f08092ff02b343c6f72221f17ab960375bff9b
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-03-14 00:11:23 +0100

    Wordlist: 1) Split at entry two (separate singular and plural). 2) Added more words. 3) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.50a939";
+            string toReturn = "1.1.50a940";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.50a939 2025-03-13T212059Z+0";
+        return "Edit Overflow v. 1.1.50a940 2025-03-13T225659Z+0";
     }
 
 
