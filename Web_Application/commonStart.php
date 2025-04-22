ea8e7b4e702acae1de3373aee04be0c8f30a1b47
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-04-22 22:04:28 +0200

    Wordlist: 1) Made the annotation for keyboards more regular (consistent). 2) Added more words.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.51a154 2025-04-22T024830Z+0";
+        return "Edit Overflow v. 1.1.51a155 2025-04-22T193604Z+0";
     }
 
 
