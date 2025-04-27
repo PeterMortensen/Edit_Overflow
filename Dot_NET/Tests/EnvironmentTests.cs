f3cdc7021a712d9842c070e44ac9bbd4fa785055
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-04-27 16:57:55 +0200

    Wordlist: Added more words.

--- Dot_NET/Tests/EnvironmentTests.cs
+++ Dot_NET/Tests/EnvironmentTests.cs
@@ -56,7 +56,7 @@ namespace OverflowHelper.Tests
             // Ensure we are actually running the expected version. But what
             // is actually our intent here? Why do we have this test?
 
-            string presumedNewest = "1.1.51a165";
+            string presumedNewest = "1.1.51a166";
 
             {
                 // For the main application
