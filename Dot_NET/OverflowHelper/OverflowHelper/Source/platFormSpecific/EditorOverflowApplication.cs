7d4e73ec69a100a46d3618bccd1fb43f861c556c
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-07-14 16:58:41 +0200

    Wordlist: 1) Added more words, in particular direct text entries for the various Keychron keyboard series. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.51a341";
+            string toReturn = "1.1.51a342";
             return toReturn;
         }
 
