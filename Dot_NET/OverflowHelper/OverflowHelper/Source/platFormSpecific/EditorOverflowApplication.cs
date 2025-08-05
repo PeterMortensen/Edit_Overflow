a4fc3396854c38b4a59c246270348bf453a95a62
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-08-05 02:53:44 +0200

    Wordlist: Continuation of consolidating the (implicit) number conversions, with some reduction in redundancy.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.51a390";
+            string toReturn = "1.1.51a391";
             return toReturn;
         }
 
