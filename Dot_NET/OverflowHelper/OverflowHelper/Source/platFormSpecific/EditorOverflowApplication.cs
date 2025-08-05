93dad64641d116e425ddd1a9b39e749308ea7457
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-08-05 05:46:43 +0200

    Wordlist: Continuation of consolidating the (implicit) number conversions, with more reduction in redundancy (most explicit decimal numbers eliminated); and we get more (automatic) cross references than before due to the implicit number conversions.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.51a391";
+            string toReturn = "1.1.51a392";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.51a391 2025-08-04T234303Z+0";
+        return "Edit Overflow v. 1.1.51a392 2025-08-05T032707Z+0";
     }
 
 
