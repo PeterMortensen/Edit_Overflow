3fe316a7f3c860f729d30dc75a60a03f971739c1
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-08-09 15:48:59 +0200

    Wordlist: 1) Added "&nbsp;" as prefix for some units. 2) Added more words. 3) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/Tests/EnvironmentTests.cs
+++ Dot_NET/Tests/EnvironmentTests.cs
@@ -56,7 +56,7 @@ namespace OverflowHelper.Tests
             // Ensure we are actually running the expected version. But what
             // is actually our intent here? Why do we have this test?
 
-            string presumedNewest = "1.1.51a403";
+            string presumedNewest = "1.1.51a404";
 
             {
                 // For the main application
