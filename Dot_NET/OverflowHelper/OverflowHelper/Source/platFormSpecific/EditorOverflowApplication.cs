6b3f4a3bfa4f53a40fd8ca2d3a6e86596595e436
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-08-30 20:37:11 +0200

    Wordlist: 1) Added more words, in particular related to unit 'kelvin'. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.51a500";
+            string toReturn = "1.1.51a501";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.51a500 2025-08-30T172112Z+0";
+        return "Edit Overflow v. 1.1.51a501 2025-08-30T183022Z+0";
     }
 
 
