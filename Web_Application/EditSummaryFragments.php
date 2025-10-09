d670bd269538e4a660303829f583dd384feeeefa
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-10-09 16:54:05 +0200

    Edit summary fragments page: 1) Slight text change. 2) Now passes HTML validation (without any warnings; there weren't any errors, only warnings).

--- Web_Application/EditSummaryFragments.php
+++ Web_Application/EditSummaryFragments.php
@@ -31,7 +31,7 @@
                 title="Shortcut: Shift + Alt + E"
             >for canned comments.</a></p>
 
-        <hr/>
+        <hr>
 
             <!--  It is not really a form - just a page where we want
                   to be able to tab between field (and )
@@ -45,7 +45,7 @@
             <p>The "How to ... ?" <u>f</u>orm for a question is not a good one
                 (does not follow
                 <a href="https://www.youtube.com/watch?v=kS5NfSzXfrI"
-                >QUASM</a>) <br/>
+                >QUASM</a>) <br>
                 <input
                     name="X25"
                     type="text"
@@ -55,11 +55,11 @@
                     style="width:1700px;"
                     accesskey="F"
                     title="Shortcut: Shift + Alt + F"
-                />
+                >
             </p>
 
             <p id="Capitalise_I">
-                Capitalise "i" - yes, <u>y</u>ou!!!!! <br/>
+                Capitalise "i" - yes, <u>y</u>ou!!!!! <br>
                 <input
                     name="X2"
                     type="text"
@@ -69,11 +69,11 @@
                     style="width:910px;"
                     accesskey="Y"
                     title="Shortcut: Shift + Alt + Y"
-                />
+                >
             </p>
 
             <p>Small formatting changes often result in
-                a large <u>d</u>iff in the default view. <br/>
+                a large <u>d</u>iff in the default view. <br>
                 <input
                     name="X7"
                     type="text"
@@ -83,7 +83,7 @@
                     style="width:820px;"
                     accesskey="D"
                     title="Shortcut: Shift + Alt + D"
-                />
+                >
             </p>
 
             <p>Meta infor<u>m</u>ation does not belong
@@ -92,7 +92,7 @@
                 <em>
                 <a href="https://meta.stackexchange.com/questions/2950/"
                 >Should 'Hi', 'thanks', taglines, and salutations be removed from posts?</a>
-                </em> <br/>
+                </em> <br>
                 <input
                     name="X14"
                     type="text"
@@ -102,12 +102,12 @@
                     style="width:400px;"
                     accesskey="M"
                     title="Shortcut: Shift + Alt + M"
-                />
+                >
             </p>
 
             <p>Statements like "the above" may make sense
                 on a f<u>o</u>rum, but
-                definitely not on Stack Exchange. <br/>
+                definitely not on Stack Exchange. <br>
                 <input
                     name="X17"
                     type="text"
@@ -117,10 +117,10 @@
                     style="width:1260px;"
                     accesskey="O"
                     title="Shortcut: Shift + Alt + O"
-                />
+                >
             </p>
 
-            <p>"<i>its"</i> vs. <i>"it's"</i> <br/>
+            <p>"<i>its"</i> vs. <i>"it's"</i> <br>
                 <input
                     name="X19"
                     type="text"
@@ -130,7 +130,7 @@
                     style="width:1220px;"
                     accesskey="X"
                     title=""
-                />
+                >
             </p>
 
             <p>
@@ -143,11 +143,11 @@
                     style="width:240px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
             <p>Links should<u>n</u>'t be naked -
-                use an appropriate link text. <br/>
+                use an appropriate link text. <br>
                 <input
                     name="X3"
                     type="text"
@@ -157,7 +157,7 @@
                     style="width:170px;"
                     accesskey="N"
                     title="Shortcut: Shift + Alt + N"
-                />
+                >
             </p>
 
             <p>
@@ -170,11 +170,11 @@
                     style="width:170px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
             <p>Many users post sloppy or even m<u>i</u>sleading
-                indentation of code. <br/>
+                indentation of code. <br>
                 <input
                     name="X5"
                     type="text"
@@ -184,11 +184,11 @@
                     style="width:150px;"
                     accesskey="I"
                     title="Shortcut: Shift + Alt + I"
-                />
+                >
             </p>
 
             <p>Shorthands or TLAs often make <u>p</u>osts too obscure for
-                the actual audience (from search engine hits). <br/>
+                the actual audience (from search engine hits). <br>
                 <input
                     name="X6"
                     type="text"
@@ -198,12 +198,12 @@
                     style="width:90px;"
                     accesskey="P"
                     title="Shortcut: Shift + Alt + P"
-                />
+                >
             </p>
 
             <p>Inappropriate shortening of "IP address"
                 (IP addresses are bloc<u>k</u>ed, not
-                the protocol). <br/>
+                the protocol). <br>
                 <input
                     name="X13"
                     type="text"
@@ -213,11 +213,11 @@
                     style="width:1600px;"
                     accesskey="K"
                     title="Shortcut: Shift + Alt + K"
-                />
+                >
             </p>
 
             <p>Capita<u>l</u>isation often obscures
-                the meaning when proper nouns are involved. <br/>
+                the meaning when proper nouns are involved. <br>
                 <input
                     name="X15"
                     type="text"
@@ -227,11 +227,11 @@
                     style="width:280px;"
                     accesskey="L"
                     title="Shortcut: Shift + Alt + L"
-                />
+                >
             </p>
 
             <p>Code formattin<u>g</u> should
-                not be used for emphasis. <br/>
+                not be used for emphasis. <br>
                 <input
                     name="X16"
                     type="text"
@@ -241,13 +241,13 @@
                     style="width:220px;"
                     accesskey="G"
                     title="Shortcut: Shift + Alt + G"
-                />
+                >
             </p>
 
             <p>Unfortunately, Stack Exchange does not
                 warn when text is
                 enclosed in "&lt;>". It is just ignored and becomes
-                invisible without warning. <br/>
+                invisible without warning. <br>
                 <input
                     name="X18"
                     type="text"
@@ -257,7 +257,7 @@
                     style="width:1050px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
             <p>Things like "Update", etc. do not
@@ -270,7 +270,7 @@
                 99% of the users for a post comes from a Google search, and
                 they have no interest whatsoever how a post changed over time -
                 they just need the best content (besides, it is ugly to have
-                "Update" in a post). <br/>
+                "Update" in a post). <br>
                 <input
                     name="X20"
                     type="text"
@@ -280,7 +280,7 @@
                     style="width:960px;"
                     accesskey="B"
                     title="Shortcut: Shift + Alt + B"
-                />
+                >
             </p>
 
             <p>It is not necessary to capitalise any words for emphasis.
@@ -291,7 +291,7 @@
                 have facilities for <b>bold</b> and <i>italics</i>.
                 It is mostly Indians who do this, but not at all exclusively.
                 Anyone who has wasted too much time with (raw text-only)
-                forums will have the tendency (and need to unlearn). <br/>
+                forums will have the tendency (and need to unlearn). <br>
                 <input
                     name="X27"
                     type="text"
@@ -301,7 +301,7 @@
                     style="width:520px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
             <!-- Or should this be in the pure links section? -->
@@ -323,7 +323,7 @@
                 (and Jeff Atwood should have said <strong><em>no</em></strong>
                 at the time).
 
-                <br/>
+                <br>
                 <input
                     name="X28"
                     type="text"
@@ -333,12 +333,12 @@
                     style="width:500px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
             <p>Rhetorical questions do not belong in an answer. One of the
                 reasons is that an answer should not address the OP - most
-                readers (e.g., 99.9999%) will be someone else. <br/>
+                readers (e.g., 99.9999%) will be someone else. <br>
                 <input
                     name="X29"
                     type="text"
@@ -348,9 +348,11 @@
                     style="width:1120px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
+            <!-- Note:  We can not use <pre>, as it introduces 
+                        line breaks. -->
             <p id="FixedSyntaxHighlighting">Most older posts
                 on Stack Overflow have strange
                 <a href="https://meta.stackexchange.com/questions/184108/what-is-syntax-highlighting-and-how-does-it-wok/184109#184109"
@@ -378,9 +380,10 @@
                     ```lang-xml,
                     ```lang-bash,
                     ```lang-typescript,
-                    ```lang-yaml, (Dockerfile)
+                    ```lang-yaml (Dockerfile), 
+                    and
                     ```lang-kotlin (.kts)
-                    <br/>
+                    <br>
                 <input
                     name="X31"
                     type="text"
@@ -390,12 +393,12 @@
                     style="width:870px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
 
             <!-- ===================================================== -->
-            <hr/>
+            <hr>
             <h2 id="PureLinks">Pure links</h2>
 
             <p id="Run-on_sentences"><a href="https://en.wikipedia.org/wiki/Sentence_clause_structure#Run-on_sentences"
@@ -403,7 +406,7 @@
                 (<a href="https://twitter.com/PeterMortensen/status/1199839973215739907"
                 >YouTube video</a>).
                 Mostly native speake<u>r</u>s (more pronounced for US native
-                speakers), but also for minimum-effort users: <br/>
+                speakers), but also for minimum-effort users: <br>
                 <input
                     name="X26"
                     type="text"
@@ -413,12 +416,12 @@
                     style="width:590px;"
                     accesskey="R"
                     title="Shortcut: Shift + Alt + R"
-                />
+                >
             </p>
 
             <p>Missing <u>a</u>rticles, Russians and Indians,
                 <a href="https://www.youtube.com/watch?v=1Dax90QyXgI&t=17m54s"
-                >&quot;a&quot;</a>: <br/>
+                >&quot;a&quot;</a>: <br>
                 <input
                     name="X10"
                     type="text"
@@ -428,12 +431,12 @@
                     style="width:410px;"
                     accesskey="A"
                     title="Shortcut: Shift + Alt + A"
-                />
+                >
             </p>
 
             <p>Missing ar<u>t</u>icles, Russians and Indians,
                 <a href="https://www.youtube.com/watch?v=1Dax90QyXgI&t=19m05s"
-                >&quot;the&quot;</a>: <br/>
+                >&quot;the&quot;</a>: <br>
                 <input
                     name="X11"
                     type="text"
@@ -443,7 +446,7 @@
                     style="width:410px;"
                     accesskey="T"
                     title="Shortcut: Shift + Alt + T"
-                />
+                >
             </p>
 
             <div id="IndianSpace"> </div>
@@ -452,7 +455,7 @@
                 (from the outdated 1935
                 <a href="https://en.wikipedia.org/wiki/Wren_%26_Martin"
                 ><i>Wren & Martin</i> textbook</a>)&mdash;keyboard
-                shortcut hint: <u>c</u>: <br/>
+                shortcut hint: <u>c</u>: <br>
                 <input
                     name="X9"
                     type="text"
@@ -462,12 +465,12 @@
                     style="width:910px;"
                     accesskey="C"
                     title="Shortcut: Shift + Alt + C"
-                />
+                >
             </p>
 
             <p id="Than-and-Then"><u>U</u>S native speakers, especially southern US,
                 <a href="https://www.wikihow.com/Use-Than-and-Then"
-                >&quot;than&quot; vs. &quot;then&quot;</a>: <br/>
+                >&quot;than&quot; vs. &quot;then&quot;</a>: <br>
                 <input
                     name="X8"
                     type="text"
@@ -477,12 +480,12 @@
                     style="width:310px;"
                     accesskey="U"
                     title="Shortcut: Shift + Alt + U"
-                />
+                >
             </p>
 
             <p>US native speakers, especially southern US,
                 <a href="https://www.wikihow.com/Use-You%27re-and-Your"
-                >&quot;you're&quot; vs. &quot;your&quot;</a>: <br/>
+                >&quot;you're&quot; vs. &quot;your&quot;</a>: <br>
                 <input
                     name="X22"
                     type="text"
@@ -492,14 +495,14 @@
                     style="width:340px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
             <p>US native speakers, especially southern US,
                 <a href="https://www.wikihow.com/Use-There,-Their-and-They%27re"
                 >&quot;there&quot; vs.
                 &quot;their&quot; vs.
-                &quot;they're&quot;</a>: <br/>
+                &quot;they're&quot;</a>: <br>
                 <input
                     name="X23"
                     type="text"
@@ -509,13 +512,13 @@
                     style="width:400px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
             <p>US native speakers, especially southern US,
                 <a href="https://www.wikihow.com/Use-Affect-and-Effect-Properly"
                 >&quot;affect&quot; vs.
-                &quot;effect&quot;</a>: <br/>
+                &quot;effect&quot;</a>: <br>
                 <input
                     name="X24"
                     type="text"
@@ -525,14 +528,14 @@
                     style="width:380px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
             <p>US native speakers, especially southern US,
                 <a href="https://www.wikihow.com/Use-Where,-Were-and-We're"
                 >&quot;where&quot; vs.
                 &quot;were&quot;vs.
-                &quot;we're&quot;</a>: <br/>
+                &quot;we're&quot;</a>: <br>
                 <input
                     name="X30"
                     type="text"
@@ -542,12 +545,12 @@
                     style="width:380px;"
                     accesskey=""
                     title=""
-                />
+                >
             </p>
 
             <p><a href="https://english.stackexchange.com/questions/15953"
                 >Space between</a>
-                <u>q</u>uantity and unit: <br/>
+                <u>q</u>uantity and unit: <br>
                 <input
                     name="X12"
                     type="text"
@@ -557,12 +560,12 @@
                     style="width:350px;"
                     accesskey="Q"
                     title="Shortcut: Shift + Alt + Q"
-                />
+                >
             </p>
 
             <p><a href="https://www.youtube.com/watch?v=1Dax90QyXgI&t=0m38s"
                 >Present simple tense, third person singular</a>
-                - t<u>h</u>ere must be an 's': <br/>
+                - t<u>h</u>ere must be an 's': <br>
                 <input
                     name="X21"
                     type="text"
@@ -572,12 +575,12 @@
                     style="width:410px;"
                     accesskey="H"
                     title="Shortcut: Shift + Alt + H"
-                />
+                >
             </p>
 
 
             <!-- Template
-            <p>Some text with <u>Z</u>s <br/>
+            <p>Some text with <u>Z</u>s <br>
 
                 <input
                     name="X"
@@ -588,7 +591,7 @@
                     style="width:110px;"
                     accesskey="Z"
                     title="Shortcut: Shift + Alt + Z"
-                />
+                >
             </p>
             -->
 
@@ -607,7 +610,7 @@
                 style="width:75px;"
                 accesskey=""
                 title=""
-            />
+            >
         </form><?php the_EditOverflowFooter('EditSummaryFragments.php', "", ""); ?>
 
 
