19eba090d97cf0d65fa60f7187d7d086257d32a6
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-10-25 22:41:30 +0200

    Wordlist: 1) Added more words, in particular chess-related. 2) Physically rearranged some items in the source, in particular chess-related. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.51a616";
+            string toReturn = "1.1.51a617";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.51a616 2025-10-25T185536Z+0";
+        return "Edit Overflow v. 1.1.51a617 2025-10-25T203655Z+0";
     }
 
 
