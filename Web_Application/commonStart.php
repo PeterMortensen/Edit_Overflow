f2ccc5ab12e4706496c87996eb3740f0e90e8b6c
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2025-11-24 22:00:18 +0100

    Wordlist: 1) Added more words, in particular chess-related. 2) Physically rearranged some items in the source, in particular chess-related. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.51a661 2025-11-24T025938Z+0";
+        return "Edit Overflow v. 1.1.51a662 2025-11-24T203436Z+0";
     }
 
 
