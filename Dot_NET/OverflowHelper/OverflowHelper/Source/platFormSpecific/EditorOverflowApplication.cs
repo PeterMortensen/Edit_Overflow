d164260b5205f207a44d8351343ae54504c307b2
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2026-01-26 23:31:28 +0100

    Wordlist: 1) Added more words. 2) Extended some annotation for some mechanical keyboards (mostly Keychron). 3) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
+++ Dot_NET/OverflowHelper/OverflowHelper/Source/platFormSpecific/EditorOverflowApplication.cs
@@ -98,7 +98,7 @@ namespace OverflowHelper
          ****************************************************************************/
         public static string versionString()
         {
-            string toReturn = "1.1.51a765";
+            string toReturn = "1.1.51a766";
             return toReturn;
         }
 
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.51a765 2026-01-26T183735Z+0";
+        return "Edit Overflow v. 1.1.51a766 2026-01-26T222457Z+0";
     }
 
 
