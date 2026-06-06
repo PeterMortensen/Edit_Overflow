37f467a0c5c441e0fb2f1d669ae3b9fb622045f6
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2026-06-06 18:20:09 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/Tests/EnvironmentTests.cs
+++ Dot_NET/Tests/EnvironmentTests.cs
@@ -62,7 +62,7 @@ namespace OverflowHelper.Tests
             //   "error CS1010: Newline in constant"
             //   "error CS1002: ; expected "
 
-            string presumedNewest = "1.1.52a056";
+            string presumedNewest = "1.1.52a057";
 
             {
                 // For the main application
--- Web_Application/commonStart.php
+++ Web_Application/commonStart.php
@@ -69,7 +69,7 @@
     #
     function get_EditOverflowID()
     {
-        return "Edit Overflow v. 1.1.52a056 2026-06-05T074607Z+0";
+        return "Edit Overflow v. 1.1.52a057 2026-06-06T155517Z+0";
     }
 
 
