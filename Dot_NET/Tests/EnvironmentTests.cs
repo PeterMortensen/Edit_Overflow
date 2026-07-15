2a585b2ad4eed0af7e6b91f5d86dd59151665c1e
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2026-07-15 12:18:12 +0200

    Wordlist: 1) Added more words. 2) Physically rearranged some items in the source. Some of a normal diff may not make much sense; only comparing the two revisions of the file in a sorted form would.

--- Dot_NET/Tests/EnvironmentTests.cs
+++ Dot_NET/Tests/EnvironmentTests.cs
@@ -62,7 +62,7 @@ namespace OverflowHelper.Tests
             //   "error CS1010: Newline in constant"
             //   "error CS1002: ; expected "
 
-            string presumedNewest = "1.1.52a110";
+            string presumedNewest = "1.1.52a111";
 
             {
                 // For the main application
