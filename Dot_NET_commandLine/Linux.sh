87ea63ea6d2c349a4f8ef9b473f38420571a8001
Peter Mortensen <spamMayGoHere@pmortensen.eu>
2026-08-01 15:47:41 +0200

    Build script: Increased some file size limit check values to reflect the growing word list.

--- Dot_NET_commandLine/Linux.sh
+++ Dot_NET_commandLine/Linux.sh
@@ -3063,7 +3063,7 @@ rm $HTML_FILE
 #       2022-01-25: 3310553 bytes
 
 # Note: The same build number
-wordListExport 33 "HTML"  $HTML_FILE   9239000 10196000
+wordListExport 33 "HTML"  $HTML_FILE   9424000 10400000
 
 
 cp  $HTML_FILE  $HTML_FILE_GENERIC
